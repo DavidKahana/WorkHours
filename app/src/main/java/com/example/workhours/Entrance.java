6b3acae8911bcc2a7aef729c5ddfa545a625b133
DavidKahana <suhs17227@gmail.com>
2023-03-14 21:44:15 +0200

    1 day - home

--- app/src/main/java/com/example/workhours/Entrance.java
+++ app/src/main/java/com/example/workhours/Entrance.java
@@ -2,13 +2,59 @@ package com.example.workhours;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
-public class Entrance extends AppCompatActivity {
+import java.util.Calendar;
 
+public class Entrance extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entrance);
+
+
+        btnDate = (Button) findViewById(R.id.btnDate);
+
+        btnDate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v==btnDate)
+        {
+            Calendar systemCalender = Calendar.getInstance();
+            int year = systemCalender.get(Calendar.YEAR);
+            int month = systemCalender.get(Calendar.MONTH);
+            int day = systemCalender.get(Calendar.DAY_OF_MONTH);
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,new SetDate(),year,month,day);
+            datePickerDialog.show();
+
+
+
+        }
+
+    }
+
+    public  class SetDate implements DatePickerDialog.OnDateSetListener
+    {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            monthOfYear = monthOfYear +1;
+
+            String str = "You selected :" + dayOfMonth + "/" + monthOfYear +"/" + year;
+            Toast.makeText(Entrance.this,str,Toast.LENGTH_LONG).show();
+            btnDate.setText(str);
+
+        }
+
     }
 }
