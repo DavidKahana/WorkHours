ee77e8189c00b49825545009e0b62c720cb4d08b
DavidKahana <suhs17227@gmail.com>
2023-03-16 20:19:08 +0200

    2 day - home

--- app/src/main/java/com/example/workhours/Entrance.java
+++ app/src/main/java/com/example/workhours/Entrance.java
@@ -3,26 +3,36 @@ package com.example.workhours;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
 
 public class Entrance extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnDate;
+    Button btnDate , btnTime , btnToStart;
+    TextView tvEntrance1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entrance);
 
 
+        tvEntrance1 = (TextView)findViewById(R.id.tvEntrance1);
+
         btnDate = (Button) findViewById(R.id.btnDate);
+        btnTime = (Button)findViewById(R.id.btnTime);
+        btnToStart = (Button)findViewById(R.id.btnToStart);
 
         btnDate.setOnClickListener(this);
+        btnTime.setOnClickListener(this);
+        btnToStart.setOnClickListener(this);
     }
 
     @Override
@@ -41,7 +51,18 @@ public class Entrance extends AppCompatActivity implements View.OnClickListener 
 
         }
 
+        else if (v == btnTime)
+        {
+            Calendar systemCalendar = Calendar.getInstance();
+            int hour = systemCalendar.get(Calendar.HOUR_OF_DAY);
+            int minute = systemCalendar.get(Calendar.MINUTE);
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,new SetYourTime(),hour,minute,true);
+            timePickerDialog.show();;
+
+
+        }
     }
+
 
     public  class SetDate implements DatePickerDialog.OnDateSetListener
     {
@@ -51,10 +72,35 @@ public class Entrance extends AppCompatActivity implements View.OnClickListener 
             monthOfYear = monthOfYear +1;
 
             String str = "You selected :" + dayOfMonth + "/" + monthOfYear +"/" + year;
-            Toast.makeText(Entrance.this,str,Toast.LENGTH_LONG).show();
+            Toast.makeText(Entrance.this,str,Toast.LENGTH_SHORT).show();
             btnDate.setText(str);
 
         }
 
+    }
+
+    public class  SetYourTime implements TimePickerDialog.OnTimeSetListener
+    {
+
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            String str= "";
+            if (hourOfDay < 10){
+                str = "Time is :" + "0" + hourOfDay +":" + minute;
+            }
+            if (minute < 10){
+                str = "Time is :" + hourOfDay +":" + "0" + minute;
+            }
+
+            if (minute < 10 && hourOfDay < 10){
+                str = "Time is :" + "0" + hourOfDay +":" + "0" + minute;
+            }
+            if (minute >= 10 && hourOfDay >= 10) {
+                str = "Time is :" + hourOfDay + ":" + minute;
+            }
+            Toast.makeText(Entrance.this,str,Toast.LENGTH_SHORT).show();
+            btnTime.setText(str);
+
+        }
     }
 }
